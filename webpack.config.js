aa6f1d878372de00d6ed1cac1169dc156c2a1098
Max Kim <hwaseen@gmail.com>
2020-05-21 22:21:44 +0900

    Add optimization property in webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: {
+    // 하나의 페이지만 생성
+    page1: "./src/index1.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    // 코드 스플리팅 할거임
+    splitChunks: {
+      chunks: "all",
+      name: "vendor",
+    },
+  },
+  plugins: [new CleanWebpackPlugin()],
+  mode: "production",
+};
