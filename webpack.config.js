2af42cdf40be0962c2b2b154a2df9742707eb0c3
Max Kim <hwaseen@gmail.com>
2020-05-23 00:17:09 +0900

    웹팩 데브서버 초기 설정, 기본 index.html 작성

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,17 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
-    // 하나의 페이지만 생성
     page1: "./src/index1.js",
   },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    hot: true,
+    port: 9000,
+  },
   optimization: {
-    // 코드 스플리팅 할거임
     splitChunks: {
       chunks: "all",
       name: "vendor",
